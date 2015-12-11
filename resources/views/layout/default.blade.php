522eb5a5975ba18595779db8911de7ab426531a0
JohJohan <johan.vlaar.1994@gmail.com>
2015-12-11 15:50:00 +0100

    search

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -22,7 +22,7 @@
 			        	<span class="icon-bar"></span>
 			        	<span class="icon-bar"></span>
 		      		</button>
-		      		<a class="navbar-brand" href="#"><img src="{!! asset('/img/sc-headerlogo-wit.png') !!}" alt="" class="img-responsive" height="50px" width="225px;"></a>
+		      		<a href="#"><img src="{!! asset('/img/sc-headerlogo-wit.png') !!}" alt="" class="img-responsive" height="50px" width="225px;"></a>
 		    	</div>
 
 		    	<!-- Collect the nav links, forms, and other content for toggling -->
@@ -30,11 +30,16 @@
 		      		<ul class="nav navbar-nav">
 		      		</ul>
 		      	<form class="navbar-form navbar-left" role="search">
-		        
-		      	</form>
+		        	<div class="input-group">
+		            	<input type="text" class="form-control" placeholder="Search" name="q">
+		            	<div class="input-group-btn">
+		                	<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+		            	</div>
+		        	</div>
+		        </form>
 		      	<ul class="nav navbar-nav navbar-right">
 		        	<li class="dropdown">
-			          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Roeland Kok <span class="caret"></span></a>
+			          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login<span class="caret"></span></a>
 			          	<ul class="dropdown-menu">
 				            <li><a href="#">Action</a></li>
 				            <li><a href="#">Another action</a></li>
@@ -43,15 +48,8 @@
 			            	<li><a href="#">Separated link</a></li>
 			          	</ul>
 		        	</li>
-		        	<li class="dropdown">
-			          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span></a>
-			          	<ul class="dropdown-menu">
-				            <li><a href="#">Action</a></li>
-				            <li><a href="#">Another action</a></li>
-				            <li><a href="#">Something else here</a></li>
-				            <li role="separator" class="divider"></li>
-			            	<li><a href="#">Separated link</a></li>
-			          	</ul>
+		        	<li>
+			          	<a href="#">Registeren</a>
 		        	</li>
 		      	</ul>
 		        	
