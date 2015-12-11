efab7f75272a9f59ea1ae959698b9809044ed082
Berwout Kruit <tehber@hotmail.com>
2015-12-11 15:07:39 +0100

    Fout opgelost.
    Fout in de controller.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,7 +17,7 @@ class AdminController extends Controller
     public function index()
     {
         //
-        return view()->route('admin.home');
+        return view('admin.home');
     }
 
     /**
