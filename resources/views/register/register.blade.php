0d0fa96a3c8a34cf36050f28855ad3dcbb87ed80
KIPdeKIP <kipdekip@gmail.com>
2016-01-15 15:05:34 +0100

    Add form to register

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -1,6 +1,18 @@
 @extends('layout.default')
-
 @section('content')
-
-    <h3>test</h3>
+<h1>Registreren</h1>
+ <form role="form">
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
 @stop
