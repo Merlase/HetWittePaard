3c6e3f54c75575395e36d245fd6f4dd954870e2c
Your Name <you@example.com>
2016-01-15 09:26:40 -0500

    aanpassingen register

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -3,19 +3,31 @@
 <h1>Registreren</h1>
  <form role="form">
   <div class="form-group required">
-    <label class="control-label" for="email">Email address:</label>
+    <label class="control-label" for="email">voornaam:</label>
     <input type="email" class="form-control" id="email">
   </div>
   <div class="form-group required">
-    <label class="control-label" for="pwd">Password:</label>
+    <label class="control-label" for="pwd">achternaam:</label>
     <input type="password" class="form-control" id="pwd">
   </div>
      <div class="form-group required">
-         <label class="control-label" for="pwd">extra:</label>
+         <label class="control-label" for="pwd">gebruikersnaam:</label>
          <input type="extra" class="form-control" id="pwd">
      </div>
      <div class="form-group">
-         <label class="control-label" for="pwd">extra:</label>
+         <label class="control-label" for="pwd">e-mail:</label>
+         <input type="extra" class="form-control" id="pwd">
+     </div>
+     <div class="form-group required">
+         <label class="control-label" for="pwd">telefoonnummer:</label>
+         <input type="extra" class="form-control" id="pwd">
+     </div>
+     <div class="form-group required">
+         <label class="control-label" for="pwd">wachtwoord:</label>
+         <input type="extra" class="form-control" id="pwd">
+     </div>
+     <div class="form-group required">
+         <label class="control-label" for="pwd">wachtwoord herhalen:</label>
          <input type="extra" class="form-control" id="pwd">
      </div>
   <div class="checkbox">
