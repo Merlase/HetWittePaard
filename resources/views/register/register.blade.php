b998c3ecefd5884fcc73bf8a9473830e1cfd0cb5
KIPdeKIP <kipdekip@gmail.com>
2016-01-22 13:26:55 +0100

    Cleanup HTML

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -1,38 +1,38 @@
 @extends('layout.default')
 @section('content')
 <h1>Registreren</h1>
- <form role="form">
-  <div class="form-group required">
-    <label class="control-label" for="email">voornaam:</label>
-    <input type="email" class="form-control" id="email">
-  </div>
-  <div class="form-group required">
-    <label class="control-label" for="pwd">achternaam:</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-     <div class="form-group required">
-         <label class="control-label" for="pwd">gebruikersnaam:</label>
-         <input type="extra" class="form-control" id="pwd">
-     </div>
-     <div class="form-group required">
-         <label class="control-label" for="pwd">e-mail:</label>
-         <input type="extra" class="form-control" id="pwd">
-     </div>
-     <div class="form-group ">
-         <label class="control-label" for="pwd">telefoonnummer:</label>
-         <input type="extra" class="form-control" id="pwd">
-     </div>
-     <div class="form-group required">
-         <label class="control-label" for="pwd">wachtwoord:</label>
-         <input type="extra" class="form-control" id="pwd">
-     </div>
-     <div class="form-group required">
-         <label class="control-label" for="pwd">wachtwoord herhalen:</label>
-         <input type="extra" class="form-control" id="pwd">
-     </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+<form role="form">
+	<div class="form-group required">
+		<label class="control-label" for="firstname">Voornaam:</label>
+		<input type="text" class="form-control" id="firstname" name="firstname" required>
+	</div>
+	<div class="form-group required">
+		<label class="control-label" for="lastname">Achternaam:</label>
+		<input type="text" class="form-control" id="lastname" name="lastname" required>
+	</div>
+	<div class="form-group required">
+		<label class="control-label" for="username">Gebruikersnaam:</label>
+		<input type="text" class="form-control" id="username" name="username" required>
+	</div>
+	<div class="form-group required">
+		<label class="control-label" for="email">E-mail:</label>
+		<input type="email" class="form-control" id="email" name="email" required>
+	</div>
+	<div class="form-group ">
+		<label class="control-label" for="tel">Telefoonnummer:</label>
+		<input type="tel" class="form-control" id="tel" name="tel" required>
+	</div>
+	<div class="form-group required">
+		<label class="control-label" for="pwd">Wachtwoord:</label>
+		<input type="password" class="form-control" id="pwd" name="pwd" required>
+	</div>
+	<div class="form-group required">
+		<label class="control-label" for="pwdc">Wachtwoord herhalen:</label>
+		<input type="password" class="form-control" id="pwdc" name="pwdc" required>
+	</div>
+	<div class="checkbox">
+		<label><input type="checkbox"> Remember me</label>
+	</div>
+	<button type="submit" class="btn btn-default">Submit</button>
 </form>
 @stop
